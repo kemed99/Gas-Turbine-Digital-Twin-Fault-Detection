437dce3a473b6a9e5cc09d8e7ee013da87b95236
kemed99 <zhafranahmed918@gmail.com>
2025-12-07 19:47:03 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using GasTurbineFaultDetector;
+using System;
+using System.Windows.Forms;
+
+namespace GasTurbineFaultDetector // Namespace ini HARUS SAMA dengan Form1.cs
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Ini baris yang tadi error. Sekarang aman karena namespace sudah sama.
+            Application.Run(new Form1());
+        }
+    }
+}
